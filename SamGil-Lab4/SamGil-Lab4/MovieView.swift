abe0efc1a1382fd6c9fb0aecbc553d63fe761514
SamEGil00 <sam.e.gil@wustl.edu>
2024-10-21 10:59:31 -0500

    Add files via upload

---
+++ SamGil-Lab4/SamGil-Lab4/MovieView.swift
@@ -0,0 +1,15 @@
+//
+//  MovieView.swift
+//  SamGil-Lab4
+//
+//  Created by Samuel Gil on 10/21/24.
+//
+
+import Foundation
+import UIKit
+
+class MovieView: UIView {
+
+    
+
+}
---
+++ SamGil-Lab4/SamGil-Lab4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SamGil-Lab4
+//
+//  Created by Samuel Gil on 10/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var movies: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var favoriteViewButton: UIButton!
+    @IBOutlet weak var movieViewButton: UIButton!
+    @IBOutlet weak var currentViewLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
